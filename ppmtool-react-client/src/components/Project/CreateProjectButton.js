4d720b16102fd33fc56faf123636a5087b6b0be7
Jingchao Zhang <jerryzhang4013@gmail.com>
2022-04-02 19:22:47 -0400

    routes

---
+++ ppmtool-react-client/src/components/Project/CreateProjectButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CreateProjectButton = () => {
+  return (
+    <React.Fragment>
+      <Link to="/addProject" className="btn btn-lg btn-info text-light">
+        Create a Project
+      </Link>
+    </React.Fragment>
+
+  );
+};
+
+export default CreateProjectButton;
