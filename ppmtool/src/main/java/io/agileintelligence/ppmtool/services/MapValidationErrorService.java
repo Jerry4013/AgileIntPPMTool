e44038341ed092a0d717ad9e057a27a16c891f1d
Jingchao Zhang <jerryzhang4013@gmail.com>
2022-04-01 23:05:00 -0400

    move error service

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/MapValidationErrorService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/MapValidationErrorService.java
@@ -1,6 +1,13 @@
 package io.agileintelligence.ppmtool.services;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Date: 2022-03-29
@@ -8,5 +15,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MapValidationErrorService {
+    public ResponseEntity<?> MapValidationService(BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
 
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
 }
