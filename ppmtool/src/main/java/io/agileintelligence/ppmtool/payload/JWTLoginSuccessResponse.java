8887781ced5c99f4dd0fe8c6a0a84bceabacc746
Jingchao Zhang <jerryzhang4013@gmail.com>
2022-04-09 10:23:08 -0400

    JWT provider pre-work

---
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/payload/JWTLoginSuccessResponse.java
@@ -0,0 +1,39 @@
+package io.agileintelligence.ppmtool.payload;
+
+/**
+ * Date: 2022-04-09
+ * Time: 10:16 AM
+ */
+public class JWTLoginSuccessResponse {
+    private boolean success;
+    private String token;
+
+    public JWTLoginSuccessResponse(boolean success, String token) {
+        this.success = success;
+        this.token = token;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String toString() {
+        return "JWTLoginSuccessResponse{" +
+                "success=" + success +
+                ", token='" + token + '\'' +
+                '}';
+    }
+}
