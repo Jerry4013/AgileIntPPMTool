75ae87c3b3927b9367f6a476d5d276048296de1c
Jingchao Zhang <jerryzhang4013@gmail.com>
2022-04-09 17:39:40 -0400

    project control

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -4,6 +4,8 @@ import io.agileintelligence.ppmtool.domain.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Date: 2022-03-29
  * Time: 9:41 PM
@@ -15,4 +17,6 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
 
     @Override
     Iterable<Project> findAll();
+
+    List<Project> findAllByProjectLeader(String username);
 }
--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -2,11 +2,16 @@ package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Backlog;
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.domain.User;
 import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
+import io.agileintelligence.ppmtool.exceptions.ProjectNotFoundException;
 import io.agileintelligence.ppmtool.repositories.BacklogRepository;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import io.agileintelligence.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Date: 2022-03-29
@@ -21,8 +26,16 @@ public class ProjectService {
     @Autowired
     private BacklogRepository backlogRepository;
 
-    public Project saveOrUpdateProject(Project project) {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Project saveOrUpdateProject(Project project, String username) {
         try {
+
+            User user = userRepository.findByUsername(username);
+
+            project.setUser(user);
+            project.setProjectLeader(user.getUsername());
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 
             if (project.getId() == null) {
@@ -42,7 +55,7 @@ public class ProjectService {
         }
     }
 
-    public Project findProjectByIdentifier(String projectId) {
+    public Project findProjectByIdentifier(String projectId, String username) {
 
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 
@@ -50,19 +63,18 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
         }
 
+        if (!project.getProjectLeader().equals(username)) {
+            throw new ProjectNotFoundException("Project not found in your account");
+        }
+
         return project;
     }
 
-    public Iterable<Project> findAllProjects() {
-        return projectRepository.findAll();
+    public List<Project> findAllProjects(String username) {
+        return projectRepository.findAllByProjectLeader(username);
     }
 
-    public void deleteProjectByIdentifier(String projectId) {
-        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
-        if (project == null) {
-            throw new ProjectIdException("Cannot Project with ID '" + projectId + "' This project does not exist.");
-        }
-
-        projectRepository.delete(project);
+    public void deleteProjectByIdentifier(String projectId, String username) {
+        projectRepository.delete(findProjectByIdentifier(projectId, username));
     }
 }
